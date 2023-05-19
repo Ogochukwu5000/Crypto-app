e0a074e7578d73b0d902554198e3eab19ab85e29
Haruna Oseni <harunaoseni23@gmail.com>
2023-05-19 01:05:57 -0500

    add: password visibility state

--- src/components/LoginScreen Component/LoginScreen.tsx
+++ src/components/LoginScreen Component/LoginScreen.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
-import {Text, Image, SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  TextInput,
+} from 'react-native';
 
 function LoginScreen(): JSX.Element {
+  const [emailOrCtag, setEmailOrCtag] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordHidden, setPasswordHidden] = useState(true);
+
   return (
     <SafeAreaView style={styles.container}>
       {/* Image */}
@@ -11,6 +22,33 @@ function LoginScreen(): JSX.Element {
         style={styles.image}
         resizeMode="contain"
       />
+      {/* Bottom half  log in modal */}
+      <View style={styles.bottomHalfLoginModal}>
+        <View style={styles.emailInput}>
+          <TextInput
+            style={styles.input}
+            placeholder="Email address or Crypto tag"
+            onChangeText={setEmailOrCtag}
+            value={emailOrCtag}
+            // specify type
+            keyboardType="email-address"
+            placeholderTextColor={'#3D4C63'}
+            textContentType="emailAddress"
+          />
+        </View>
+        <View style={styles.passwordInput}>
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            onChangeText={setPassword}
+            value={password}
+            // specify type
+            keyboardType="default"
+            placeholderTextColor={'#3D4C63'}
+            secureTextEntry={passwordHidden}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -31,6 +69,33 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: 'bold',
     marginTop: '5%',
+  },
+  bottomHalfLoginModal: {
+    backgroundColor: '#fff',
+    width: '100%',
+    height: '60%',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    alignItems: 'center',
+  },
+
+  input: {
+    width: '100%',
+    height: 50,
+    marginTop: '5%',
+    borderBottomWidth: 1,
+    borderColor: '#D8D8D8',
+    fontSize: 20,
+  },
+
+  emailInput: {
+    width: '90%',
+    display: 'flex',
+  },
+
+  passwordInput: {
+    width: '90%',
+    display: 'flex',
   },
 });
 
