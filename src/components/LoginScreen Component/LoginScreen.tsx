85c1f260286083933d62e4225daecdd441aecbfc
Haruna Oseni <harunaoseni23@gmail.com>
2023-05-18 23:35:06 -0500

    fix: modified code to safe area view

--- src/components/LoginScreen Component/LoginScreen.tsx
+++ src/components/LoginScreen Component/LoginScreen.tsx
@@ -1,44 +1,36 @@
-import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  Image,
-  TextInput,
-  KeyboardAvoidingView,
-  TouchableOpacity,
-  SafeAreaView,
-  StyleSheet,
-} from 'react-native';
+import React from 'react';
+import {Text, Image, SafeAreaView, StyleSheet} from 'react-native';
 
 function LoginScreen(): JSX.Element {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       {/* Image */}
       <Text style={styles.welcomeHeader}>Welcome Back!</Text>
-      <Image source={require('../../assets/social.png')} style={styles.image} />
-    </View>
+      <Image
+        source={require('../../assets/social.png')}
+        style={styles.image}
+        resizeMode="contain"
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#3447F0',
-    height: '100%',
-    width: '100%',
     alignItems: 'center',
   },
   image: {
     width: '90%',
     height: '40%',
-    alignSelf: 'center',
     marginTop: '5%',
-    objectFit: 'contain',
   },
   welcomeHeader: {
     color: '#fff',
     fontSize: 30,
     fontWeight: 'bold',
-    marginTop: '20%',
+    marginTop: '5%',
   },
 });
 
