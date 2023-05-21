10d3b55cf31cbbdbbcb138ef7a9d63b965c0c774
Haruna Oseni <harunaoseni23@gmail.com>
2023-05-20 19:25:22 -0500

    add: clear pin on pin wrong

--- src/components/PinVerificationScreen Component/PinVerificationScreen.tsx
+++ src/components/PinVerificationScreen Component/PinVerificationScreen.tsx
@@ -8,7 +8,9 @@ import {
   Dimensions,
   TouchableOpacity,
   Animated,
+  ActivityIndicator,
 } from 'react-native';
+import Loading from '../LoadingScreen Component/LoadingScreen';
 
 const {width} = Dimensions.get('window');
 const isSmallScreen = width < 400; // Adjust the width value based on the screen size you consider as small
@@ -17,6 +19,7 @@ function PinVerification(): JSX.Element {
   const [pin, setPin] = useState('');
   const [pinCount, setPinCount] = useState(0);
   const shakeAnimation = useRef(new Animated.Value(0)).current;
+  const [isLoading, setIsLoading] = useState(true);
 
   const handlePinKeyPress = (digit: string) => {
     if (digit === 'X') {
@@ -71,117 +74,125 @@ function PinVerification(): JSX.Element {
   }
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Image
-          source={require('../../assets/back.png')}
-          style={[styles.image, isSmallScreen && styles.smallScreenImage]}
-          resizeMode="contain"
-        />
-        <View style={styles.headerText}>
-          <Text
+      {!isLoading ? (
+        <>
+          <View style={styles.header}>
+            <Image
+              source={require('../../assets/back.png')}
+              style={[styles.image, isSmallScreen && styles.smallScreenImage]}
+              resizeMode="contain"
+            />
+            <View style={styles.headerText}>
+              <Text
+                style={[
+                  styles.verificationHeader,
+                  isSmallScreen && styles.smallScreenVerificationHeader,
+                ]}>
+                Verification Required
+              </Text>
+              <Text
+                style={[
+                  styles.verificationSubHeader,
+                  isSmallScreen && styles.smallScreenVerificationSubHeader,
+                ]}>
+                Please enter your PIN to proceed
+              </Text>
+            </View>
+          </View>
+          <Animated.View
             style={[
-              styles.verificationHeader,
-              isSmallScreen && styles.smallScreenVerificationHeader,
+              styles.pinContainer,
+              isSmallScreen && styles.smallScreenPinContainer,
+              isPinWrong && {
+                transform: [{translateX: shakeAnimation}],
+              },
             ]}>
-            Verification Required
-          </Text>
-          <Text
+            <View style={[styles.pin, pinCount >= 1 && styles.pinFilled]} />
+            <View style={[styles.pin, pinCount >= 2 && styles.pinFilled]} />
+            <View style={[styles.pin, pinCount >= 3 && styles.pinFilled]} />
+            <View style={[styles.pin, pinCount >= 4 && styles.pinFilled]} />
+          </Animated.View>
+          {/* Pin key pad */}
+          <View
             style={[
-              styles.verificationSubHeader,
-              isSmallScreen && styles.smallScreenVerificationSubHeader,
+              styles.pinKeyPad,
+              isSmallScreen && styles.smallScreenPinKeyPad,
             ]}>
-            Please enter your PIN to proceed
-          </Text>
+            <View style={styles.pinKeyPadRow}>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('1')}>
+                <Text style={styles.pinKeyPadButtonText}>1</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('2')}>
+                <Text style={styles.pinKeyPadButtonText}>2</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('3')}>
+                <Text style={styles.pinKeyPadButtonText}>3</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.pinKeyPadRow}>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('4')}>
+                <Text style={styles.pinKeyPadButtonText}>4</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('5')}>
+                <Text style={styles.pinKeyPadButtonText}>5</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('6')}>
+                <Text style={styles.pinKeyPadButtonText}>6</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.pinKeyPadRow}>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('7')}>
+                <Text style={styles.pinKeyPadButtonText}>7</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('8')}>
+                <Text style={styles.pinKeyPadButtonText}>8</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('9')}>
+                <Text style={styles.pinKeyPadButtonText}>9</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.pinKeyPadRow}>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('.')}>
+                <Text style={styles.pinKeyPadButtonText}>.</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('0')}>
+                <Text style={styles.pinKeyPadButtonText}>0</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.pinKeyPadButton}
+                onPress={() => handlePinKeyPress('X')}>
+                <Text style={styles.pinKeyPadButtonText}>X</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </>
+      ) : (
+        <View>
+          <Loading />
         </View>
-      </View>
-      <Animated.View
-        style={[
-          styles.pinContainer,
-          isSmallScreen && styles.smallScreenPinContainer,
-          isPinWrong && {
-            transform: [{translateX: shakeAnimation}],
-          },
-        ]}>
-        <View style={[styles.pin, pinCount >= 1 && styles.pinFilled]} />
-        <View style={[styles.pin, pinCount >= 2 && styles.pinFilled]} />
-        <View style={[styles.pin, pinCount >= 3 && styles.pinFilled]} />
-        <View style={[styles.pin, pinCount >= 4 && styles.pinFilled]} />
-      </Animated.View>
-      {/* Pin key pad */}
-      <View
-        style={[
-          styles.pinKeyPad,
-          isSmallScreen && styles.smallScreenPinKeyPad,
-        ]}>
-        <View style={styles.pinKeyPadRow}>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('1')}>
-            <Text style={styles.pinKeyPadButtonText}>1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('2')}>
-            <Text style={styles.pinKeyPadButtonText}>2</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('3')}>
-            <Text style={styles.pinKeyPadButtonText}>3</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.pinKeyPadRow}>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('4')}>
-            <Text style={styles.pinKeyPadButtonText}>4</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('5')}>
-            <Text style={styles.pinKeyPadButtonText}>5</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('6')}>
-            <Text style={styles.pinKeyPadButtonText}>6</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.pinKeyPadRow}>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('7')}>
-            <Text style={styles.pinKeyPadButtonText}>7</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('8')}>
-            <Text style={styles.pinKeyPadButtonText}>8</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('9')}>
-            <Text style={styles.pinKeyPadButtonText}>9</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.pinKeyPadRow}>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('.')}>
-            <Text style={styles.pinKeyPadButtonText}>.</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('0')}>
-            <Text style={styles.pinKeyPadButtonText}>0</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.pinKeyPadButton}
-            onPress={() => handlePinKeyPress('X')}>
-            <Text style={styles.pinKeyPadButtonText}>X</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      )}
     </SafeAreaView>
   );
 }
