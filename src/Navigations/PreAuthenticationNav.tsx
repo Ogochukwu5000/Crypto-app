68a949a630c1fdf403adefe4c20f17c395f5b181
Haruna Oseni <harunaoseni23@gmail.com>
2023-05-20 20:40:05 -0500

    add: forgotpassword components

--- src/Navigations/PreAuthenticationNav.tsx
+++ src/Navigations/PreAuthenticationNav.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import LoginScreen from '../components/LoginScreen Component/LoginScreen';
+import PinVerification from '../components/PinVerificationScreen Component/PinVerificationScreen';
+import ForgotPassword from '../components/ForgotPasswordScreen Component/ForgotPasswordScreen';
 
 function PreAuthenticationNav(): JSX.Element {
   const Stack = createStackNavigator();
 
   return (
     <Stack.Navigator>
-      <Stack.Screen
+      {/* <Stack.Screen
         name="Login"
         component={LoginScreen}
+        options={{headerShown: false}}
+      /> */}
+      {/* <Stack.Screen
+        name="PinVerification"
+        component={PinVerification}
+        options={{headerShown: false}}
+      /> */}
+      <Stack.Screen
+        name="ForgotPassword"
+        component={ForgotPassword}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
