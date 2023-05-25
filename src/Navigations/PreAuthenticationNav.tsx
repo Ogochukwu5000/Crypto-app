9096399c3ccc8ad00aa62ad462d747a31a9f241e
Haruna Oseni <harunaoseni23@gmail.com>
2023-05-25 02:56:38 -0500

    add: create new password component

--- src/Navigations/PreAuthenticationNav.tsx
+++ src/Navigations/PreAuthenticationNav.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import PinVerification from '../components/PinVerificationScreen Component/PinVerificationScreen';
 import ForgotPassword from '../components/ForgotPasswordScreen Component/ForgotPasswordScreen';
 import CheckEmailScreen from '../components/CheckEmailScreen Component/CheckEmailScreen';
+import CreateNewPassword from '../components/CreateNewPasswordScreen Component/CreateNewPassword';
 
 function PreAuthenticationNav(): JSX.Element {
   const Stack = createStackNavigator();
@@ -24,10 +25,15 @@ function PreAuthenticationNav(): JSX.Element {
         component={ForgotPassword}
         options={{headerShown: false}}
       /> */}
-      <Stack.Screen
+      {/* <Stack.Screen
         name="CheckEmail"
         component={CheckEmailScreen}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
+      /> */}
+      <Stack.Screen
+        name="CreateNewPassword"
+        component={CreateNewPassword}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
