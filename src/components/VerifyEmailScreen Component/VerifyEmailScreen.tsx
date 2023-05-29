8e50461b480d26e3b22333362645fd33e01c99e5
Haruna Oseni <harunaoseni23@gmail.com>
2023-05-29 03:46:15 -0500

    add: loading condition

--- src/components/VerifyEmailScreen Component/VerifyEmailScreen.tsx
+++ src/components/VerifyEmailScreen Component/VerifyEmailScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     Text,
     Image,
@@ -9,59 +9,113 @@ import {
     Linking
 } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store/reducers';
+import { useNavigation } from '@react-navigation/native';
+import Loading from '../LoadingScreen Component/LoadingScreen';
 
 const { width } = Dimensions.get('window');
 const isSmallScreen = width < 400; // Adjust the width value based on the screen size you consider as small
 
 const handleOpenEmailApp = () => {
-    // Open Email App
-    Linking.openURL('mailto:');
+    Linking.openURL('https://mail.google.com/');
 }
 
-
 function VerifyEmailScreen(): JSX.Element {
+    const user = useSelector((state: RootState) => state.userReducer.user);
+    const navigation = useNavigation();
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    useEffect(() => {
+        const handleDeepLink = (url: string) => {
+            const route = url.replace(/.*?:\/\//g, '');
+            const routeName = route.split('/')[1];
+            if (routeName === 'verify-email') {
+                setIsLoading(true);
+                setTimeout(() => {
+                    setIsLoading(false);
+                    navigation.navigate('Welcome' as never);
+                }, 2000);
+            }
+        }
+
+        Linking.getInitialURL().then((url) => {
+            if (url) {
+                handleDeepLink(url);
+            }
+        });
+
+        const handleUrl = ({ url }: { url: string }) => {
+            handleDeepLink(url);
+        };
+        (Linking as any).addEventListener('url', handleUrl);
+        return () => {
+            (Linking as any).removeEventListener('url', handleUrl);
+        };
+    }, []);
+
     return (
         <SafeAreaView style={styles.container}>
-            {/* Image */}
-            <View style={styles.header}>
-                <Image
-                    source={require('../../assets/back.png')}
-                    style={[styles.image, isSmallScreen && styles.smallScreenImage]}
-                    resizeMode="contain"
-                />
-                <View style={styles.headerText}>
-                    <Text
-                        style={[
-                            styles.checkScreenHeader,
-                            isSmallScreen && styles.smallScreenCheckScreenHeader,
-                        ]}>
-                        Verify Email
-                    </Text>
-                    <Text
-                        style={[
-                            styles.checkScreenSubHeader,
-                            isSmallScreen && styles.smallScreenCheckScreenSubHeader,
-                        ]}>
-                        Just one last thing, lets verify that gorgeous email of yours!{' '}
-                    </Text>
-                </View>
-            </View>
-            <Image
-                source={require('../../assets/email.png')}
-                style={[
-                    styles.emailImage,
-                    isSmallScreen && styles.smallScreenEmailImage,
-                ]}
-                resizeMode="contain"
-            />
-            <TouchableOpacity
-                onPress={handleOpenEmailApp}
-                style={[
-                    styles.resetButton,
-                    isSmallScreen && styles.smallScreenResetButton,
-                ]}>
-                <Text style={styles.resetButtonText}>Open Email App</Text>
-            </TouchableOpacity>
+            {
+                isLoading ? (
+                    <>
+                        {/* Image */}
+                        <View style={styles.header}>
+                            <Image
+                                source={require('../../assets/back.png')}
+                                style={[styles.image, isSmallScreen && styles.smallScreenImage]}
+                                resizeMode="contain"
+                            />
+                            <View style={styles.headerText}>
+                                <Text
+                                    style={[
+                                        styles.checkScreenHeader,
+                                        isSmallScreen && styles.smallScreenCheckScreenHeader,
+                                    ]}>
+                                    Verify Email
+                                </Text>
+                                <Text
+                                    style={[
+                                        styles.checkScreenSubHeader,
+                                        isSmallScreen && styles.smallScreenCheckScreenSubHeader,
+                                    ]}>
+                                    Just one last thing, lets verify that gorgeous email of yours!{' '}
+                                </Text>
+                            </View>
+                        </View>
+                        <Image
+                            source={require('../../assets/email.png')}
+                            style={[
+                                styles.emailImage,
+                                isSmallScreen && styles.smallScreenEmailImage,
+                            ]}
+                            resizeMode="contain"
+                        />
+                        <TouchableOpacity
+                            onPress={() => {
+                                axios.post('http://localhost:8000/user/sendverification', {
+                                    email: user?.email,
+                                }).then((response) => {
+                                    console.log('Response: ', response.data);
+                                }).catch((error) => {
+                                    console.log('Error: ', error);
+                                });
+                                handleOpenEmailApp();
+                            }}
+                            style={[
+                                styles.resetButton,
+                                isSmallScreen && styles.smallScreenResetButton,
+                            ]}>
+                            <Text style={styles.resetButtonText}>Open Email App</Text>
+                        </TouchableOpacity>
+                    </>
+                ) : (
+                    <View>
+                        <Loading />
+                    </View>
+                )
+            }
         </SafeAreaView>
     );
 }
