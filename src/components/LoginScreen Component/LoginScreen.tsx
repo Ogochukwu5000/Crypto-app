c51acecf64301b4e45e830d2d6c626e4cce36713
Haruna Oseni <harunaoseni23@gmail.com>
2023-05-29 23:37:15 -0500

    fix: error with Alert

--- src/components/LoginScreen Component/LoginScreen.tsx
+++ src/components/LoginScreen Component/LoginScreen.tsx
@@ -7,9 +7,13 @@ import {
   View,
   TextInput,
   KeyboardAvoidingView,
+  Alert
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+import Loading from '../LoadingScreen Component/LoadingScreen';
 
 function LoginScreen(): JSX.Element {
   const [emailOrCtag, setEmailOrCtag] = useState('');
@@ -17,6 +21,53 @@ function LoginScreen(): JSX.Element {
   const [passwordHidden, setPasswordHidden] = useState(true);
   const [isFocused, setIsFocused] = useState(false);
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const cryptoTagRegex = /^[a-zA-Z0-9]+$/;
+    let isEmail = false;
+    let isCryptoTag = false;
+
+    if (emailRegex.test(emailOrCtag)) {
+      isEmail = true;
+    } else if (cryptoTagRegex.test(emailOrCtag)) {
+      isCryptoTag = true;
+    } else {
+      // handle invalid input
+      return;
+    }
+
+    axios.post('http://10.0.0.174:8000/user/login', {
+      email: isEmail ? emailOrCtag : '',
+      crypto_tag: isCryptoTag ? emailOrCtag.startsWith("#") ? emailOrCtag : "#" + emailOrCtag : '',
+      password,
+    }).then((response) => {
+      if (response.status) {
+        console.log('Response: ', response.data);
+        setIsLoading(true);
+        setTimeout(() => {
+          setIsLoading(false);
+          dispatch({
+            type: 'LOGIN',
+            payload: {
+              email: response.data.user.email,
+              cryptoTag: response.data.user.crypto_tag,
+              token: response.data.token,
+              firstName: response.data.user.first_name,
+              lastName: response.data.user.last_name,
+            },
+          });
+        }
+          , 2000);
+      } else {
+        Alert.alert('Invalid email or password');
+      }
+    }).catch((error) => {
+      Alert.alert('Invalid email or password');
+    });
+  };
 
 
   const handleFocus = () => {
@@ -34,68 +85,75 @@ function LoginScreen(): JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
       {/* Image */}
-      <Text style={styles.welcomeHeader}>Welcome Back!</Text>
-      <Image
-        source={require('../../assets/social.png')}
-        style={styles.image}
-        resizeMode="contain"
-      />
-      {/* Bottom half  log in modal */}
-      <KeyboardAvoidingView
-        style={[styles.bottomHalfLoginModal, isFocused && styles.focusedInput]}>
-        <View style={styles.emailInput}>
-          <TextInput
-            style={styles.input}
-            placeholder="Email address or Crypto tag"
-            onChangeText={setEmailOrCtag}
-            value={emailOrCtag}
-            // specify type
-            keyboardType="email-address"
-            placeholderTextColor={'#3D4C63'}
-            textContentType="emailAddress"
-            onFocus={handleFocus}
-            onSubmitEditing={handleBlur}
-          />
-        </View>
-        <View style={styles.passwordInput}>
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            onChangeText={setPassword}
-            value={password}
-            keyboardType="default"
-            placeholderTextColor={'#3D4C63'}
-            secureTextEntry={passwordHidden}
-            onFocus={handleFocus}
-            onSubmitEditing={handleBlur}
-          />
-          <TouchableOpacity
-            style={styles.passwordEye}
-            onPress={togglePasswordHidden}>
-            {
-              // if password is hidden show text "show" else show text "hide"
-              passwordHidden ? (
-                <Text style={styles.passwordEyeText}>Show</Text>
-              ) : (
-                <Text style={styles.passwordEyeText}>Hide</Text>
-              )
-            }
-          </TouchableOpacity>
-        </View>
-        <TouchableOpacity style={styles.forgotPasswordButton} onPress={() => navigation.navigate('ForgotPassword' as never)}>
-          <Text style={styles.forgotPasswordBottonText}>Forgot Password?</Text>
-        </TouchableOpacity>
-        <View style={styles.loginFooter}>
-          <TouchableOpacity style={styles.loginButton}>
-            <Text style={styles.loginButtonText}>Login</Text>
-          </TouchableOpacity>
-          {/* Dont have an account sign up */}
-          <TouchableOpacity style={styles.signUpButton} onPress={() => navigation.navigate('Signup' as never)}>
-            <Text style={styles.signUpButtonText}>Don't have an account?</Text>
-            <Text style={styles.signUpLinkText}> Sign up</Text>
-          </TouchableOpacity>
-        </View>
-      </KeyboardAvoidingView>
+      {
+        !isLoading ?
+          (<>
+            <Text style={styles.welcomeHeader}>Welcome Back!</Text>
+            <Image
+              source={require('../../assets/social.png')}
+              style={styles.image}
+              resizeMode="contain"
+            />
+            {/* Bottom half  log in modal */}
+            <KeyboardAvoidingView
+              style={[styles.bottomHalfLoginModal, isFocused && styles.focusedInput]}>
+              <View style={styles.emailInput}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Email address or Crypto tag"
+                  onChangeText={setEmailOrCtag}
+                  value={emailOrCtag}
+                  // specify type
+                  keyboardType="email-address"
+                  placeholderTextColor={'#3D4C63'}
+                  textContentType="emailAddress"
+                  onFocus={handleFocus}
+                  onSubmitEditing={handleBlur}
+                />
+              </View>
+              <View style={styles.passwordInput}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Password"
+                  onChangeText={setPassword}
+                  value={password}
+                  keyboardType="default"
+                  placeholderTextColor={'#3D4C63'}
+                  secureTextEntry={passwordHidden}
+                  onFocus={handleFocus}
+                  onSubmitEditing={handleBlur}
+                />
+                <TouchableOpacity
+                  style={styles.passwordEye}
+                  onPress={togglePasswordHidden}>
+                  {
+                    // if password is hidden show text "show" else show text "hide"
+                    passwordHidden ? (
+                      <Text style={styles.passwordEyeText}>Show</Text>
+                    ) : (
+                      <Text style={styles.passwordEyeText}>Hide</Text>
+                    )
+                  }
+                </TouchableOpacity>
+              </View>
+              <TouchableOpacity style={styles.forgotPasswordButton} onPress={() => navigation.navigate('ForgotPassword' as never)}>
+                <Text style={styles.forgotPasswordBottonText}>Forgot Password?</Text>
+              </TouchableOpacity>
+              <View style={styles.loginFooter}>
+                <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+                  <Text style={styles.loginButtonText}>Login</Text>
+                </TouchableOpacity>
+                {/* Dont have an account sign up */}
+                <TouchableOpacity style={styles.signUpButton} onPress={() => navigation.navigate('Signup' as never)}>
+                  <Text style={styles.signUpButtonText}>Don't have an account?</Text>
+                  <Text style={styles.signUpLinkText}> Sign up</Text>
+                </TouchableOpacity>
+              </View>
+            </KeyboardAvoidingView>
+          </>) : (<View>
+            <Loading />
+          </View>)
+      }
     </SafeAreaView>
   );
 }
