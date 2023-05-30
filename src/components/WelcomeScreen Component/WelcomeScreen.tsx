f31b520de3762a1362ec3a2c5f65adc317de2176
Haruna Oseni <harunaoseni23@gmail.com>
2023-05-29 21:45:12 -0500

    add: register and auth dispatch

--- src/components/WelcomeScreen Component/WelcomeScreen.tsx
+++ src/components/WelcomeScreen Component/WelcomeScreen.tsx
@@ -11,11 +11,34 @@ import {
     UIActivityIndicator,
     WaveIndicator,
 } from 'react-native-indicators';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../../store/reducers';
 
 const { width } = Dimensions.get('window');
 const isSmallScreen = width < 400; // Adjust the width value based on the screen size you consider as small
 
 function Welcome(): JSX.Element {
+    const dispatch = useDispatch();
+    const user = useSelector((state: RootState) => state.userReducer.user);
+    useEffect(() => {
+        axios.post('http://10.0.0.174:8000/user/register', {
+            email: user?.email,
+            first_name: user?.firstName,
+            last_name: user?.lastName,
+            password: user?.password,
+            pin: user?.pin,
+            crypto_tag: user?.cryptoTag,
+        }).then((response) => {
+            console.log('Response: ', response.data);
+            dispatch({ type: 'AUTHENTICATE' });
+        }
+        ).catch((error) => {
+            console.log('Error: ', error);
+        }
+        );
+    }, []);
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content}>
