f95e8bc40005e201b2084a92aa489e2a57c81e7c
Haruna Oseni <harunaoseni23@gmail.com>
2023-05-30 10:55:06 -0500

    import: user redux state

--- src/components/CreateNewPasswordScreen Component/CreateNewPassword.tsx
+++ src/components/CreateNewPasswordScreen Component/CreateNewPassword.tsx
@@ -10,6 +10,10 @@ import {
   Dimensions,
 } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../../store/reducers';
 
 const { width } = Dimensions.get('window');
 const isSmallScreen = width < 400; // Adjust the width value based on the screen size you consider as small
@@ -20,6 +24,9 @@ function CreateNewPassword(): JSX.Element {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [passwordHidden, setPasswordHidden] = useState(true);
   const [passwordConfirmHidden, setPasswordConfirmHidden] = useState(true);
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.userReducer.user);
 
   const handleFocus = () => {
     setIsFocused(true);
