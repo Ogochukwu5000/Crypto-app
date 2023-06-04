86cb36ff6f85060bf12fff65407bf5891c3e92b0
Haruna Oseni <harunaoseni23@gmail.com>
2023-06-04 10:42:14 -0500

    add: search component

---
+++ src/components/SearchScreen Component/SearchScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, SafeAreaView } from 'react-native';
+
+function Search(): JSX.Element {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>This is the search screen</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default Search;
