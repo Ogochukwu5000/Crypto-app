bfeb87ffb5ea690cdf2d63dfb0ea43070d91eebf
Haruna Oseni <harunaoseni23@gmail.com>
2023-06-04 10:41:10 -0500

    add: crypto app screen and home screen

---
+++ src/components/CryptoAppScreen Component/CryptoAppScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, SafeAreaView } from 'react-native';
+
+function CryptoApp(): JSX.Element {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>This is the cryptoapp screen</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default CryptoApp;
---
+++ src/components/HomeScreen Component/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, SafeAreaView } from 'react-native';
+
+function Home(): JSX.Element {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>This is the home screen</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default Home;
