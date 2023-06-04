18724a15d33c61166590556121ef63cd29911d9f
Haruna Oseni <harunaoseni23@gmail.com>
2023-06-04 10:47:55 -0500

    add: activity screen

---
+++ src/components/ActivityScreen Component/ActivityScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, SafeAreaView } from 'react-native';
+
+function Activity(): JSX.Element {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>This is the activity screen</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default Activity;
