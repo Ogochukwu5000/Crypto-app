514e091b49e55586776a48b6d798e1fc72a000ab
Haruna Oseni <harunaoseni23@gmail.com>
2023-06-05 19:33:40 -0500

    add: crypto app screen

--- src/components/CryptoAppScreen Component/CryptoAppScreen.tsx
+++ src/components/CryptoAppScreen Component/CryptoAppScreen.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Text, View, SafeAreaView } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
 
 function CryptoApp(): JSX.Element {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
             <View>
                 <Text>This is the cryptoapp screen</Text>
             </View>
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#3447F0',
+        alignItems: 'center',
+    },
+});
 
 export default CryptoApp;
