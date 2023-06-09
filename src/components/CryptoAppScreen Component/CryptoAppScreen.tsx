7e676179681298a1815f1db8b9f7087ce36b4e4e
Haruna Oseni <harunaoseni23@gmail.com>
2023-06-09 01:28:58 -0500

    choose recipient screen

--- src/components/CryptoAppScreen Component/CryptoAppScreen.tsx
+++ src/components/CryptoAppScreen Component/CryptoAppScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import CryptoAppMain from './CryptoAppMain';
+import ChooseRecipientScreen from './ChooseRecipientScreen';
 
 function CryptoApp(): JSX.Element {
     const Stack = createStackNavigator();
@@ -9,6 +10,11 @@ function CryptoApp(): JSX.Element {
             <Stack.Screen
                 name="CryptoAppMainScreen"
                 component={CryptoAppMain}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="ChooseRecipientScreen"
+                component={ChooseRecipientScreen}
                 options={{ headerShown: false }}
             />
         </Stack.Navigator>
