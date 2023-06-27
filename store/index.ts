c9774c907edd88830aabf2fd12a845a5e1d20aba
Haruna Oseni <harunaoseni23@gmail.com>
2023-06-27 16:59:41 -0500

    making redux persistent

--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,17 @@
 import { legacy_createStore as createStore, Store } from 'redux';
 import rootReducer, { RootState } from './reducers';
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store: Store<RootState> = createStore(rootReducer);
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store: Store<RootState> = createStore(persistedReducer);
+
+export const persistor = persistStore(store);
 
 export default store;
