f971805dd3b4287e47b80fa45065dfef9a044a42
Haruna Oseni <harunaoseni23@gmail.com>
2023-07-01 11:03:48 -0500

    replaced image

--- src/components/OnboardingScreen Component/OnboardingScreen1.tsx
+++ src/components/OnboardingScreen Component/OnboardingScreen1.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import {
+    Text,
+    Image,
+    SafeAreaView,
+    StyleSheet,
+    View,
+    TouchableOpacity
+} from 'react-native';
 
 function OnboardingScreen1(): JSX.Element {
     return (
         <SafeAreaView style={styles.container}>
-            <Text>OnboardingScreen1</Text>
+            <TouchableOpacity style={styles.headerContainer}><Text style={styles.Header}>Skip</Text></TouchableOpacity>
+            <Image
+                source={require('../../assets/desktop.png')}
+                style={styles.image}
+                resizeMode="contain"
+            />
+            <View style={[styles.bottomHalfModal]}>
+            </View>
         </SafeAreaView>
     );
 }
@@ -12,9 +26,32 @@ function OnboardingScreen1(): JSX.Element {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#ffff',
-        height: '100%',
+        backgroundColor: '#3447F0',
+        alignItems: 'center',
+    },
+    image: {
+        width: '90%',
+        height: '30%',
+        marginTop: '8%',
+    },
+    Header: {
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: '500',
+        padding: 10,
+    },
+    headerContainer: {
+        alignSelf: 'flex-end',
+    },
+    bottomHalfModal: {
+        backgroundColor: '#fff',
         width: '100%',
+        height: '63%',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 0,
     },
 });
 
