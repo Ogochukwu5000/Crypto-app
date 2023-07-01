1e65f6d7b18a9a2fad772e68a05fc5445e8d4be3
Haruna Oseni <harunaoseni23@gmail.com>
2023-07-01 10:45:43 -0500

    adding onboarding screens to stack

--- src/Navigations/GettingStarted.tsx
+++ src/Navigations/GettingStarted.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import OnboardingScreen1 from '../components/OnboardingScreen Component/OnboardingScreen1';
+import OnboardingScreen2 from '../components/OnboardingScreen Component/OnboardingScreen2';
+import OnboardingScreen3 from '../components/OnboardingScreen Component/OnboardingScreen3';
+import OnboardingScreen4 from '../components/OnboardingScreen Component/OnboardingScreen4';
 
 function GettingStarted(): JSX.Element {
     const Stack = createStackNavigator();
@@ -9,6 +12,21 @@ function GettingStarted(): JSX.Element {
             <Stack.Screen
                 name="OnboardingScreen1"
                 component={OnboardingScreen1}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="OnboardingScreen2"
+                component={OnboardingScreen2}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="OnboardingScreen3"
+                component={OnboardingScreen3}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="OnboardingScreen4"
+                component={OnboardingScreen4}
                 options={{ headerShown: false }}
             />
         </Stack.Navigator>
