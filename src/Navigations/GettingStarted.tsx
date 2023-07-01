f2bf6404915a90602fb5a4479bb568bf27abca7a
Haruna Oseni <harunaoseni23@gmail.com>
2023-07-01 10:11:38 -0500

    created getting started component

---
+++ src/Navigations/GettingStarted.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function GettingStarted(): JSX.Element {
+    const Stack = createStackNavigator();
+    return (
+        <>
+        </>
+    )
+}
+
+export default GettingStarted;
