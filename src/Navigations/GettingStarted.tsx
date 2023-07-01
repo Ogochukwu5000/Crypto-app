b07d42c203b680ee032828a41f5214fd16a67d77
Haruna Oseni <harunaoseni23@gmail.com>
2023-07-01 10:36:07 -0500

    onboarding screen one

--- src/Navigations/GettingStarted.tsx
+++ src/Navigations/GettingStarted.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import OnboardingScreen1 from '../components/OnboardingScreen Component/OnboardingScreen1';
 
 function GettingStarted(): JSX.Element {
     const Stack = createStackNavigator();
     return (
-        <>
-        </>
+        <Stack.Navigator>
+            <Stack.Screen
+                name="OnboardingScreen1"
+                component={OnboardingScreen1}
+                options={{ headerShown: false }}
+            />
+        </Stack.Navigator>
     )
 }
 
