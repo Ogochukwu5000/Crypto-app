2a5730b9dbbdf0450ab6466e8ce92af5ff219cfe
Haruna Oseni <harunaoseni23@gmail.com>
2023-07-01 22:56:47 -0500

    remove unused import and initial styling

--- src/components/ActivityScreen Component/ActivityMainScreen.tsx
+++ src/components/ActivityScreen Component/ActivityMainScreen.tsx
@@ -1,24 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
     Text,
     Image,
     SafeAreaView,
     StyleSheet,
     View,
-    Dimensions,
-    TouchableOpacity,
-    FlatList,
 } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-
-const { width } = Dimensions.get('window');
-const isSmallScreen = width < 400; // Adjust the width value based on the screen size you consider as small
 
 function ActivityMain(): JSX.Element {
 
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.headerText}>All Activity</Text>
+            <View style={styles.transactionContainer}>
+                <View style={styles.transactionImage}>
+                    <Image
+                        source={require('../../assets/arrow-up-right-circle.png')}
+                        style={[styles.image]}
+                        resizeMode="contain"
+                    />
+                </View>
+                <View>
+                    <Text style={styles.transactionAmount}>$ 100</Text>
+                    <Text style={styles.transactionCryptoAmount}>0.0001 BTC</Text>
+                </View>
+                <View>
+                    <Text style={styles.transactionType}>Sent</Text>
+                    <Text style={styles.transactionDate}>Aug 19, 2023</Text>
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
@@ -32,6 +42,42 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: '500',
         marginTop: 20,
+        marginBottom: 50,
+    },
+    transactionContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        width: '90%',
+        height: 80,
+        backgroundColor: '#fff',
+        borderRadius: 50,
+        marginTop: 20,
+    },
+    transactionImage: {
+        width: 50,
+    },
+    image: {
+        width: 30,
+        height: 50,
+        objectFit: 'contain',
+    },
+    transactionAmount: {
+        fontWeight: 'bold',
+        fontSize: 15,
+    },
+    transactionCryptoAmount: {
+        fontSize: 15,
+        color: '#AFAFAF',
+    },
+    transactionType: {
+        fontWeight: 'bold',
+        color: '#3447F0',
+        fontSize: 15,
+    },
+    transactionDate: {
+        color: '#AFAFAF',
+        fontSize: 15,
     },
 });
 
