af1dbf7f88cf266b185a9036105f21ccd36ebdf9
Haruna Oseni <harunaoseni23@gmail.com>
2023-07-01 23:06:08 -0500

    flat list for activity implemented

--- src/components/ActivityScreen Component/ActivityMainScreen.tsx
+++ src/components/ActivityScreen Component/ActivityMainScreen.tsx
@@ -5,30 +5,77 @@ import {
     SafeAreaView,
     StyleSheet,
     View,
+    TouchableOpacity,
+    FlatList,
 } from 'react-native';
 
+type Transaction = {
+    id: string;
+    amount: string;
+    cryptoAmount: string;
+    type: string;
+    date: string;
+    image: any;
+};
+
+const transactions: Transaction[] = [
+    {
+        id: '1',
+        amount: '$ 100',
+        cryptoAmount: '0.0001 BTC',
+        type: 'Sent',
+        date: 'Aug 19, 2023',
+        image: require('../../assets/arrow-up-right-circle.png'),
+    },
+    {
+        id: '2',
+        amount: '$ 50',
+        cryptoAmount: '0.00005 BTC',
+        type: 'Sent',
+        date: 'Aug 18, 2023',
+        image: require('../../assets/arrow-up-right-circle.png'),
+    },
+    {
+        id: '3',
+        amount: '$ 200',
+        cryptoAmount: '0.0002 BTC',
+        type: 'Sent',
+        date: 'Aug 17, 2023',
+        image: require('../../assets/arrow-up-right-circle.png'),
+    },
+];
+
 function ActivityMain(): JSX.Element {
+
+    const renderItem = ({ item }: { item: Transaction }) => (
+        <TouchableOpacity style={styles.transactionContainer}>
+            <View style={styles.transactionImage}>
+                <Image
+                    source={item.image}
+                    style={[styles.image]}
+                    resizeMode="contain"
+                />
+            </View>
+            <View>
+                <Text style={styles.transactionAmount}>{item.amount}</Text>
+                <Text style={styles.transactionCryptoAmount}>{item.cryptoAmount}</Text>
+            </View>
+            <View>
+                <Text style={styles.transactionType}>{item.type}</Text>
+                <Text style={styles.transactionDate}>{item.date}</Text>
+            </View>
+        </TouchableOpacity>
+    );
 
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.headerText}>All Activity</Text>
-            <View style={styles.transactionContainer}>
-                <View style={styles.transactionImage}>
-                    <Image
-                        source={require('../../assets/arrow-up-right-circle.png')}
-                        style={[styles.image]}
-                        resizeMode="contain"
-                    />
-                </View>
-                <View>
-                    <Text style={styles.transactionAmount}>$ 100</Text>
-                    <Text style={styles.transactionCryptoAmount}>0.0001 BTC</Text>
-                </View>
-                <View>
-                    <Text style={styles.transactionType}>Sent</Text>
-                    <Text style={styles.transactionDate}>Aug 19, 2023</Text>
-                </View>
-            </View>
+            <FlatList
+                data={transactions}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+                contentContainerStyle={styles.listContainer}
+            />
         </SafeAreaView>
     )
 }
@@ -44,15 +91,26 @@ const styles = StyleSheet.create({
         marginTop: 20,
         marginBottom: 50,
     },
+    listContainer: {
+        alignItems: 'center',
+    },
     transactionContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-evenly',
         width: '90%',
         height: 80,
-        backgroundColor: '#fff',
+        backgroundColor: '#EDF1F9',
         borderRadius: 50,
         marginTop: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
     },
     transactionImage: {
         width: 50,
@@ -64,22 +122,21 @@ const styles = StyleSheet.create({
     },
     transactionAmount: {
         fontWeight: 'bold',
-        fontSize: 15,
+        fontSize: 18,
     },
     transactionCryptoAmount: {
-        fontSize: 15,
+        fontSize: 18,
         color: '#AFAFAF',
     },
     transactionType: {
         fontWeight: 'bold',
         color: '#3447F0',
-        fontSize: 15,
+        fontSize: 18,
     },
     transactionDate: {
         color: '#AFAFAF',
-        fontSize: 15,
+        fontSize: 18,
     },
 });
-
 
 export default ActivityMain;
