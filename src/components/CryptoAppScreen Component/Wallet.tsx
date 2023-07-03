97a58b7e2f95458b054c04a4be62259d44160e4a
Haruna Oseni <harunaoseni23@gmail.com>
2023-07-03 09:05:17 -0500

    removed unused variables and imports

---
+++ src/components/CryptoAppScreen Component/Wallet.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import {
+    Text,
+    Image,
+    SafeAreaView,
+    StyleSheet,
+    View,
+    KeyboardAvoidingView,
+    Dimensions,
+} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store/reducers';
+
+const { width } = Dimensions.get('window');
+const isSmallScreen = width < 400; // Adjust the width value based on the screen size you consider as small
+
+function Wallet(): JSX.Element {
+
+    const user = useSelector((state: RootState) => state.userReducer.user);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity>
+                    <Image
+                        source={require('../../assets/back.png')}
+                        style={[styles.image, isSmallScreen && styles.smallScreenImage]}
+                        resizeMode="contain"
+                    />
+                </TouchableOpacity>
+                <View style={styles.headerText}>
+                    <Text
+                        style={[
+                            styles.Header,
+                            isSmallScreen && styles.smallScreenHeader,
+                        ]}>
+                        Personal Information
+                    </Text>
+                    <Text
+                        style={[
+                            styles.SubHeader,
+                            isSmallScreen && styles.smallScreenSubHeader,
+                        ]}>
+                        We would love to know you more, we think you’re amazing so why not tell us a bit about yourself :)
+                    </Text>
+                </View>
+            </View>
+            {/* Bottom half  log in modal */}
+            <KeyboardAvoidingView
+                style={[styles.bottomHalfModal, isSmallScreen && styles.isSmallBottomHalfModal]}>
+
+            </KeyboardAvoidingView>
+        </SafeAreaView >
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#3447F0',
+        alignItems: 'center',
+    },
+    bottomHalfModal: {
+        backgroundColor: '#fff',
+        width: '100%',
+        height: '85%',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 0,
+    },
+    isSmallBottomHalfModal: {
+        height: '80%',
+    },
+    header: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: '3%',
+        marginTop: '3%',
+    },
+    image: {
+        width: 30,
+        height: 100,
+    },
+    smallScreenImage: {
+        width: 30,
+        height: 80,
+    },
+    Header: {
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginLeft: '5%',
+        width: '100%',
+    },
+    headerText: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        marginLeft: '5%',
+    },
+    smallScreenHeader: {
+        fontSize: 25,
+    },
+    SubHeader: {
+        color: '#fff',
+        fontSize: 15,
+        width: '80%',
+        marginTop: '2%',
+        textAlign: 'center',
+    },
+    smallScreenSubHeader: {
+        fontSize: 13,
+        marginLeft: '3%',
+        width: '70%',
+    },
+});
+
+export default Wallet;
