d94ddffdb55f05174b2f0c380fcba735d8eb82e4
Haruna Oseni <harunaoseni23@gmail.com>
2023-07-04 14:42:43 -0500

    add plugins for rn

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  presets: ['babel-preset-expo'],
+  plugins: ['react-native-reanimated/plugin'],
 };
+
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//     plugins: ['react-native-reanimated/plugin'],
+//   };
+// };
