3874b6fd2d28e32f1e4aff0ca4317b25ee920788
Haruna Oseni <harunaoseni23@gmail.com>
2023-07-09 12:46:17 -0500

    added source Sxts

--- metro.config.js
+++ metro.config.js
@@ -5,12 +5,17 @@
  * @format
  */
 
+const extraNodeModules = require('node-libs-browser');
 module.exports = {
+  resolver: {
+    extraNodeModules,
+    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'json'] //add here
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true,
+        inlineRequires: false,
       },
     }),
   },
