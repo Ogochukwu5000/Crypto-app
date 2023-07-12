78fb3d73c4b0db85ef5f9cc7808d503f48bd052e
Haruna Oseni <harunaoseni23@gmail.com>
2023-07-11 19:04:01 -0500

    remove ether library

--- src/components/CryptoAppScreen Component/Wallet.tsx
+++ src/components/CryptoAppScreen Component/Wallet.tsx
@@ -13,7 +13,6 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import { WalletConnectModal, useWalletConnectModal } from '@walletconnect/modal-react-native';
 import { TextEncoder, TextDecoder } from 'text-encoding';
-import { ethers } from 'ethers';
 
 const { width } = Dimensions.get('window');
 const isSmallScreen = width < 400; // Adjust the width value based on the screen size you consider as small
@@ -40,22 +39,74 @@ function Wallet(): JSX.Element {
     console.log('Address: ', address);
     console.log('IsConnected: ', isConnected);
 
+
+
     // Request the balance
-    const getBalance = async () => {
-        try {
-            // Make the balance request
-            const balance = await provider?.request({
-                method: 'eth_getBalance',
-                params: [address, 'latest'],
-            });
+    // const getBalance = async () => {
+    //     try {
+    //         // Make the balance request
+    //         const balance = await provider?.request({
+    //             method: 'eth_getBalance',
+    //             params: [address, 'latest'],
+    //         });
 
-            console.log('Balance: ', balance);
-        } catch (error) {
-            console.error('Error getting balance:', error);
-        }
-    };
+    //         console.log('Balance: ', balance);
+    //     } catch (error) {
+    //         console.error('Error getting balance:', error);
+    //     }
+    // };
 
-    //getBalance();
+    const usdtContractAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7'; // USDT contract address
+    const usdcContractAddress = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'; // USDC contract address
+
+    // const getERC20Balance = async (tokenContractAddress: any, walletAddress: any) => {
+    //     try {
+    //         const balance = await provider?.request({
+    //             method: 'eth_call',
+    //             params: [
+    //                 {
+    //                     to: tokenContractAddress,
+    //                     data: `0x70a08231000000000000000000000000${walletAddress.slice(2)}`,
+    //                 },
+    //                 'latest',
+    //             ],
+    //         });
+
+    //         console.log(`Balance of ${tokenContractAddress}: ${balance}`);
+
+    //         const balanceInWei = ethers.from(balance).toString();
+    //         // const balanceInEther = ethers.utils.formatEther(balanceInWei);
+
+    //         // console.log(`Balance of ${tokenContractAddress}: ${balanceInEther}`);
+    //     } catch (error) {
+    //         console.error(`Error getting balance of ${tokenContractAddress}:`, error);
+    //     }
+    // };
+
+    // // Call the function to get the balances
+    // getERC20Balance(usdtContractAddress, address);
+    // getERC20Balance(usdcContractAddress, address);
+
+
+    // const handleSendTransaction = async () => {
+    //     if (isConnected && provider) {
+    //         const tx = {
+    //             from: address,
+    //             to: '0x1234567890123456789012345678901234567890', // Replace with the recipient address
+    //             value: ethers.parseEther('0.1').toString(), // Replace with the amount to send
+    //         };
+
+    //         try {
+    //             const txHash = await provider?.request({
+    //                 method: 'eth_sendTransaction',
+    //                 params: [tx],
+    //             });
+    //             txHash && console.log('TxHash: ', txHash);
+    //         } catch (error) {
+    //             console.error(error);
+    //         }
+    //     }
+    // };
 
     const wallets = [
         // { name: 'Metamask', address: '', image: require('../../assets/metamask.png') },
@@ -110,6 +161,8 @@ function Wallet(): JSX.Element {
                         setSelectedWallet('');
                         if (name === 'Wallet Connect') {
                             provider?.disconnect();
+                            // clean up
+                            close();
                         }
                     },
                 },
