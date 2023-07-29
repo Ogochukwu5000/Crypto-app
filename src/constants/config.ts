a19a361f46a48283d67efac71d3d418fa22570f6
Haruna Oseni <harunaoseni23@gmail.com>
2023-07-29 14:59:16 -0500

    add constant for api

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,1 +1,3 @@
-const BASE_URL = `http://127.0.0.1:8080`;
+export const BASE_URL = `http://127.0.0.1:8080`;
+export const COIN_GECO_API_PROD = `https://pro-api.coingecko.com/api/v3/`;
+export const COIN_GECO_API_DEV = `https://api.coingecko.com/api/v3/`;
