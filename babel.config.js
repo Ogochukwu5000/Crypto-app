d0ac7e1eb6af180639c27961add25832a1c1964f
Haruna Oseni <harunaoseni23@gmail.com>
2023-08-05 13:52:52 -0500

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  presets: ['babel-preset-expo'],
-  plugins: ['react-native-reanimated/plugin'],
+  presets: ['module:metro-react-native-babel-preset', 'babel-preset-expo'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+  ],
 };
