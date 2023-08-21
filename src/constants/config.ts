fec9349338ce75c64f46a62a8d907f7979e1db1e
Haruna Oseni <harunaoseni23@gmail.com>
2023-08-21 00:09:14 -0500

    added a /

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
-export const DEV_URL = `http://10.0.0.174:8000`;
-export const PROD_URL = `https://cryptoapp-frontend.herokuapp.com`;
+export const DEV_URL = `http://10.0.0.174:8000/`;
+export const PROD_URL = `https://cryptoapp-frontend.herokuapp.com/`;
 export const BASE_URL = DEV_URL;
 export const COIN_GECO_API_PROD = `https://pro-api.coingecko.com/api/v3/`;
 export const COIN_GECO_API_DEV = `https://api.coingecko.com/api/v3/`;
