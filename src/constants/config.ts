f5983ab58050f79a61bc9d0b1f02a91230bbdad3
Haruna Oseni <harunaoseni23@gmail.com>
2023-08-21 00:01:56 -0500

    add dev and prod url

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,6 @@
-export const BASE_URL = `http://127.0.0.1:8080`;
+export const DEV_URL = `http://127.0.0.1:8080`;
+export const PROD_URL = `https://cryptoapp-frontend.herokuapp.com`;
+export const BASE_URL = DEV_URL;
 export const COIN_GECO_API_PROD = `https://pro-api.coingecko.com/api/v3/`;
 export const COIN_GECO_API_DEV = `https://api.coingecko.com/api/v3/`;
 export const COIN_GECO_API = COIN_GECO_API_DEV;
