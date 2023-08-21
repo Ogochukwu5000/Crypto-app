2f5979bfe1af7ff6e2162d37c3c5d4f8629e2001
Haruna Oseni <harunaoseni23@gmail.com>
2023-08-21 00:03:40 -0500

    corrected dev url

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
-export const DEV_URL = `http://127.0.0.1:8080`;
+export const DEV_URL = `http://10.0.0.174:8000`;
 export const PROD_URL = `https://cryptoapp-frontend.herokuapp.com`;
 export const BASE_URL = DEV_URL;
 export const COIN_GECO_API_PROD = `https://pro-api.coingecko.com/api/v3/`;
