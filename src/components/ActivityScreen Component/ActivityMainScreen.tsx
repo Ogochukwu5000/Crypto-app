a352a4e9b69ca02185a24dcee3047ade696c0e06
Haruna Oseni <harunaoseni23@gmail.com>
2023-09-29 12:21:34 -0500

    no activity at the moment

--- src/components/ActivityScreen Component/ActivityMainScreen.tsx
+++ src/components/ActivityScreen Component/ActivityMainScreen.tsx
@@ -41,38 +41,43 @@ import { BASE_URL } from '../../constants/config';
 */
 
 function ActivityMain(): JSX.Element {
-    const [transactions, setTransactions] = useState();
+    const [transactions, setTransactions] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [refreshing, setRefreshing] = useState(false); // Add refreshing state
+    const [refreshing, setRefreshing] = useState(false);
     const navigation = useNavigation();
     const user = useSelector((state: RootState) => state.userReducer.user);
+
     const renderItem = ({ item }: any) => (
-        <TouchableOpacity style={styles.transactionContainer} key={item._id} onPress={() => {
-            // @ts-ignore
-            navigation.navigate('ActivityTransactionDetails', {
-                transaction: item,
-            });
-        }}>
+        <TouchableOpacity
+            style={styles.transactionContainer}
+            key={item._id}
+            onPress={() => {
+                // @ts-ignore
+                navigation.navigate("ActivityTransactionDetails", {
+                    transaction: item,
+                });
+            }}
+        >
             <View style={styles.transactionImage}>
-                {
-                    item.is_sender ? (
-                        <Image
-                            source={require(`../../assets/${`send`}.png`)}
-                            style={[styles.image]}
-                            resizeMode="cover"
-                        />
-                    ) : (
-                        <Image
-                            source={require(`../../assets/${`Receive`}.png`)}
-                            style={[styles.image]}
-                            resizeMode="cover"
-                        />
-                    )
-                }
+                {item.is_sender ? (
+                    <Image
+                        source={require(`../../assets/${`send`}.png`)}
+                        style={[styles.image]}
+                        resizeMode="cover"
+                    />
+                ) : (
+                    <Image
+                        source={require(`../../assets/${`Receive`}.png`)}
+                        style={[styles.image]}
+                        resizeMode="cover"
+                    />
+                )}
             </View>
             <View>
                 <Text style={styles.transactionAmount}>${item.amount_usd}</Text>
-                <Text style={styles.transactionCryptoAmount}>{item.amount_crypto.toFixed(5)} ETH</Text>
+                <Text style={styles.transactionCryptoAmount}>
+                    {item.amount_crypto.toFixed(5)} ETH
+                </Text>
             </View>
             <View>
                 <Text style={styles.transactionType}>{item.time}</Text>
@@ -80,78 +85,95 @@ function ActivityMain(): JSX.Element {
             </View>
         </TouchableOpacity>
     );
+
     useEffect(() => {
         setLoading(true);
-        axios.get(`${BASE_URL}transaction/${user?.cryptoTag}`).then(response => {
-            setTransactions(response.data.transactions);
-            setLoading(false);
-        }).catch(error => {
-            console.error(JSON.stringify(error));
-            setLoading(false);
-        });
+        axios
+            .get(`${BASE_URL}transaction/${user?.cryptoTag}`)
+            .then((response) => {
+                setTransactions(response.data.transactions);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error(JSON.stringify(error));
+                setLoading(false);
+            });
     }, []);
 
     const onRefresh = () => {
-        setRefreshing(true); // Set refreshing state to true when the user pulls down the list
-        axios.get(`${BASE_URL}transaction/${user?.cryptoTag}`).then(response => {
-            setTransactions(response.data.transactions);
-            setRefreshing(false); // Set refreshing state to false after the API call is complete
-        }).catch(error => {
-            console.error(JSON.stringify(error));
-            setRefreshing(false); // Set refreshing state to false if there is an error
-        });
+        setRefreshing(true);
+        axios
+            .get(`${BASE_URL}transaction/${user?.cryptoTag}`)
+            .then((response) => {
+                setTransactions(response.data.transactions);
+                setRefreshing(false);
+            })
+            .catch((error) => {
+                console.error(JSON.stringify(error));
+                setRefreshing(false);
+            });
+    };
+
+    const renderTransactions = () => {
+        if (transactions.length === 0) {
+            return <Text style={styles.noActivity}>No Activity At The Moment</Text>;
+        } else {
+            return (
+                <FlatList
+                    data={transactions}
+                    renderItem={renderItem}
+                    keyExtractor={(item: any) => item._id}
+                    contentContainerStyle={styles.listContainer}
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={refreshing}
+                            onRefresh={onRefresh}
+                            colors={["#3447F0"]}
+                            tintColor={"#3447F0"}
+                        />
+                    }
+                />
+            );
+        }
     };
 
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.headerText}>All Activity</Text>
-            <FlatList
-                data={transactions}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.id}
-                contentContainerStyle={styles.listContainer}
-                refreshControl={ // Add refreshControl prop to the FlatList component
-                    <RefreshControl
-                        refreshing={refreshing}
-                        onRefresh={onRefresh}
-                        colors={['#3447F0']}
-                        tintColor={'#3447F0'}
-                    />
-                }
-            />
-            {loading && !refreshing && ( // Display the loading spinner if loading state is true and refreshing state is false
+            {renderTransactions()}
+            {loading && !refreshing && (
                 <View style={styles.loadingContainer}>
                     <ActivityIndicator size="large" color="#3447F0" />
                 </View>
             )}
         </SafeAreaView>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
+        alignItems: "center",
     },
     headerText: {
         fontSize: 30,
-        fontWeight: '500',
+        fontWeight: "500",
         marginTop: 20,
         marginBottom: 10,
     },
     listContainer: {
-        alignItems: 'center',
+        alignItems: "center",
     },
     transactionContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        width: '95%',
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        width: "95%",
         height: 80,
-        backgroundColor: '#EDF1F9',
+        backgroundColor: "#EDF1F9",
         borderRadius: 50,
         marginTop: 20,
-        shadowColor: '#000',
+        shadowColor: "#000",
         shadowOffset: {
             width: 0,
             height: 2,
@@ -166,35 +188,40 @@ const styles = StyleSheet.create({
     image: {
         width: 30,
         height: 50,
-        objectFit: 'contain',
+        objectFit: "contain",
     },
     transactionAmount: {
-        fontWeight: 'bold',
+        fontWeight: "bold",
         fontSize: 18,
     },
     transactionCryptoAmount: {
         fontSize: 18,
-        color: '#AFAFAF',
+        color: "#AFAFAF",
     },
     transactionType: {
-        fontWeight: 'bold',
-        color: '#3447F0',
+        fontWeight: "bold",
+        color: "#3447F0",
         fontSize: 18,
     },
     transactionDate: {
-        color: '#AFAFAF',
+        color: "#AFAFAF",
         fontSize: 18,
     },
     loadingContainer: {
-        position: 'absolute',
+        position: "absolute",
         top: 0,
         bottom: 0,
         left: 0,
         right: 0,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(255, 255, 255, 0.5)",
         zIndex: 1,
+    },
+    noActivity: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginTop: 20,
     },
 });
 
