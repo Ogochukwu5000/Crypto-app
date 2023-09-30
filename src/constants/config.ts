43d3598140ca857c5e21f648d37d8a25a4a4a4cc
Haruna Oseni <harunaoseni23@gmail.com>
2023-09-30 15:32:48 -0500

    switch to dev for the main time

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,4 +3,4 @@ export const PROD_URL = `https://crypto-app-labs-95430b345ec4.herokuapp.com/`;
 export const BASE_URL = PROD_URL;
 export const COIN_GECO_API_PROD = `https://pro-api.coingecko.com/api/v3/`;
 export const COIN_GECO_API_DEV = `https://api.coingecko.com/api/v3/`;
-export const COIN_GECO_API = COIN_GECO_API_PROD;
+export const COIN_GECO_API = COIN_GECO_API_DEV;
